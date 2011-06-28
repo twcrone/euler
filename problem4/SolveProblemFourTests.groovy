cc41a087d8d0cd20499bd7851cea3372c5f689e4
Todd Crone <twcrone@gmail.com>
2011-06-28 17:07:27 -0400

    Solved with Groovy

--- problem4/SolveProblemFourTests.groovy
+++ problem4/SolveProblemFourTests.groovy
@@ -1,22 +1,37 @@
 class Test extends GroovyTestCase {
 
-	def getPalindromicCount(String numberString) {
-		if(numberString.equals(numberString.reverse())) {
-			return numberString.length()
-		}
-		else {
-			return 0
-		}
-	}
-
-
-	void test_default_case() {
-		def count = getPalindromicCount("9009")
-		assertEquals 4, count
+	def isPalindromicNumber(num) {
+		def numStr = num.toString()
+		numStr.equals(numStr.reverse())	
 	}
 	
-	void test_non_palindromic_string_returns_zero() {
-		def count = getPalindromicCount("9099")
-		assertEquals 0, count
+	def findLargestPalindromicNumber(digits) {
+		def upperBound = 10 ** digits
+		def answer = 0
+		def temp = 0
+		(0..upperBound).each { i ->
+			(0..upperBound).each { j-> 
+				temp = i * j
+				if(temp > answer && isPalindromicNumber(temp)) {
+					answer = temp
+				}
+			} 
+		}
+		return answer
+	}
+
+	void test_default_case() {
+		def count = findLargestPalindromicNumber(2)
+		assertEquals 9009, count
+	}
+	
+	void test_is_palindromic_number() {
+		assertTrue isPalindromicNumber(10101)
+		assertFalse isPalindromicNumber(1011)
+	}
+	
+	void test_get_answer() {
+		def count = findLargestPalindromicNumber(3)
+		println count
 	}
 }
