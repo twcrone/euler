26c15e03d789195a18dbc8074cf71f4945c10e4e
Todd Crone <twcrone@gmail.com>
2011-06-28 18:26:48 -0400

    Diff test succeeds

--- problem6/SolveProblemSixTest.groovy
+++ problem6/SolveProblemSixTest.groovy
@@ -12,6 +12,17 @@ class SolveProblemSixTest extends GroovyTestCase {
 		(1..upperBound).each { sum += it }
 		return sum ** 2
 	}
+	
+	def diffSums(upperBound) {
+		def sumSquares = sumSquares(upperBound)
+		def squareSum = squareSum(upperBound)
+		squareSum - sumSquares
+	}
+
+	void test_difference_of_sums() {
+		def diff = diffSums(10)
+		assertEquals 2640, diff
+	}
 
 	void test_sum_of_squares() {
 		def sumSqs = sumSquares(10)
