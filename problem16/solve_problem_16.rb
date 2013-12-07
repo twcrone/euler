75e200a1dade8b1dac58d5ff41da5c27ba262299
Todd Crone <toddcrone@lexmark.com>
2013-12-07 18:44:01 -0500

    Solve the base case.

---
+++ problem16/solve_problem_16.rb
@@ -0,0 +1,20 @@
+require "test/unit"
+
+class SolveProblem16Tests < Test::Unit::TestCase
+
+  def solve(num)
+    num_s = num.to_s
+    sum = 0
+    num_s.each_char do |digit_s|
+      digit = digit_s.to_i
+      sum += digit
+    end
+    sum
+  end
+
+  def test_solve_base_case
+    num = 2 ** 15
+    answer = solve(num)
+    assert_equal 26, answer
+  end
+end
