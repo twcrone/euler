111c68fdfb9f2d55937de6a2a8f65c8d9f32d58c
Todd Crone <todd.crone@perceptivesoftware.com>
2014-06-02 15:29:19 -0400

    Refactor tests.

--- problem19/solve_problem_19.rb
+++ problem19/solve_problem_19.rb
@@ -7,26 +7,19 @@ class SundayCalculator
 end
 
 describe SundayCalculator, "#sunday?" do
-  
-  let(:calc) { SundayCalculator.new() }
-  let(:data) do 
-    [
-      # year, month, day_of_month, expected_is_sunday
-      [ 1900,  1,  1, false],
-      [ 1900,  1,  6, true]
-    ]
-  end
 
-  it "returns false for Jan 1, 1900" do
-    data.each do |row|
-      year = row[0]
-      month = row[1]
-      day = row[2]
-      expected = row[3]
+  let(:calc) { SundayCalculator.new() }
+
+  [
+    # year, month, day_of_month, expected_is_sunday
+    [ 1900,  1,  1, false],
+    [ 1900,  1,  6, true ]
+
+  ].each do | year, month, day, expected |
+    it "returns #{expected} for #{year}-#{month}-#{day} " do
       actual = calc.sunday?(year, month, day)
       expect(actual).to eq(expected)
     end
-
   end
-
+  
 end
