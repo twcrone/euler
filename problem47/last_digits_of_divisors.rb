8ed61bcc78406851bc1569fa1c454a62ac4f23f3
Todd Crone <todd.crone@perceptivesoftware.com>
2014-06-03 23:48:27 -0400

    Add divisors method.

--- problem47/last_digits_of_divisors.rb
+++ problem47/last_digits_of_divisors.rb
@@ -9,6 +9,16 @@ class LastDigitsOfDivisors
     n.downto(1).inject(:*)
   end
 
+  def divisors(n)
+    divisors = [1]
+    (2..n).each do |i|
+      if n % i == 0
+        divisors << i
+      end
+    end
+    divisors
+  end
+
 end
 
 describe LastDigitsOfDivisors do
@@ -23,11 +33,25 @@ describe LastDigitsOfDivisors do
       # [ factorial(50), 123, 17888],
 
     ].each do | n, d, expected |
-      it "returns #{expected} for n=#{n} and d=#{d}" do
+      it "should return #{expected} for n=#{n} and d=#{d}" do
         actual = calc.find(n, d)
         expect(actual).to eq(expected)
       end
     end
   end
 
+  context "#divisors" do
+    [
+      # integer n, digits d, expected
+      [ 84, [1, 2, 3, 4, 6, 7, 12, 14, 21, 28, 42, 84]],
+      # [ factorial(12),  12,    11],
+      # [ factorial(50), 123, 17888],
+
+    ].each do | n, divisors |
+      it "should return #{divisors} for n=#{n}" do
+        actual = calc.divisors(n)
+        expect(actual).to eq(divisors)
+      end
+    end
+  end
 end
