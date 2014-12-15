150fabe53ab4397820028c26fb90bac6af7f3a3b
Todd Crone <todd.crone@perceptivesoftware.com>
2014-12-14 21:01:39 -0500

    Cleanup Clojure solution to #2

--- problem2/problem2.clj
+++ problem2/problem2.clj
@@ -1,14 +1,8 @@
 (def four-mil (* 4 1000000))
 
-(defn even [n] (= (mod n 2) 0))
-
-(defn add-even [n] (if (even n) n 0))
-
 (defn fib [i j sum]
-  (if (even j)
-    (println j))
   (if (> j four-mil)
-    (println "DONE -> " sum)
-    (fib j (+ i j) (+ sum (add-even j)))))
+    (println sum)
+    (fib j (+ i j) (if (even? j) (+ sum j) sum))))
 
 (fib 1 2 0)
