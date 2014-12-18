5d73e42530b592714d7569e14d54bab6355866d8
Todd Crone <todd.crone@perceptivesoftware.com>
2014-12-18 13:07:32 -0500

    Refactor solution a bit

--- problem1/solve-problem-1.clj
+++ problem1/solve-problem-1.clj
@@ -1,11 +1,7 @@
-(defn get-value-div-by-5-or-3 [num] 
-  (if (or (= (mod num 3) 0) (= (mod num 5) 0)) 
-    num 
-    0))
+(defn sum-factors-of-5-or-3 [max]
+  (loop [sum 0 x max]
+    (if (pos? x)
+      (recur (if (or (= (mod x 3) 0) (= (mod x 5) 0)) (+ sum x) sum) (dec x))
+      sum)))
 
-(defn sum-factors-of-5-or-3 [sum x] 
-  (if (pos? x) 
-    (recur (+ sum (get-value-div-by-5-or-3 x)) (dec x)) 
-    sum))
-
-(println (sum-factors-of-5-or-3 0 999) "Should be" 233168)
+(println (sum-factors-of-5-or-3 999) "Should be" 233168)
