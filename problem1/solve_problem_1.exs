4aabb82e77fd51f5807f1b15852ef4c74168792e
Todd Crone <todd.crone@perceptivesoftware.com>
2015-03-11 08:45:27 -0400

    More better solution

--- problem1/solve_problem_1.exs
+++ problem1/solve_problem_1.exs
@@ -2,25 +2,15 @@ defmodule EulerProblemOne do
 
   def solve(n) when n == 1, do: IO.puts(1)
 
-  def solve(n) when rem(n, 5) and rem(n, 3) == 0 do
-    IO.puts("FizzBuzz")
+  def solve(n) when n > 1 do
+    IO.puts output(n, rem(n, 5) == 0, rem(n, 3) == 0)
     solve(n - 1)
   end
 
-  def solve(n) when rem(n, 5) == 0 do
-    IO.puts("Fizz")
-    solve(n - 1)
-  end
-
-  def solve(n) when rem(n, 3) == 0 do
-    IO.puts("Buzz")
-    solve(n - 1)
-  end
-
-  def solve(n) do
-    IO.puts(n)
-    solve(n - 1)
-  end
+  def output(_, true, true), do: "FizzBuzz"
+  def output(_, true, false), do: "Fizz"
+  def output(_, false, true), do: "Buzz"
+  def output(n, _, _), do: n
 end
 
-EulerProblemOne.solve(15)
+EulerProblemOne.solve(100)
