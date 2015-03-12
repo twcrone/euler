590dcef9d56e54a6e2665c4716054887fc4e8af9
Todd Crone <todd.crone@perceptivesoftware.com>
2015-03-11 21:19:00 -0400

    Better Elixir FizzBuzz

--- problem1/solve_problem_1.exs
+++ problem1/solve_problem_1.exs
@@ -1,8 +1,8 @@
 defmodule EulerProblemOne do
 
-  def solve(n) when n == 1, do: IO.puts(1)
+  def solve(1), do: IO.puts(1)
 
-  def solve(n) when n > 1 do
+  def solve(n) do
     IO.puts output(n, rem(n, 5) == 0, rem(n, 3) == 0)
     solve(n - 1)
   end
