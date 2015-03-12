c808aa5963a62d35dc3e5bafb3195b0359c3e5cb
Todd Crone <todd.crone@perceptivesoftware.com>
2015-03-11 22:13:50 -0400

    Make internal methods private

--- problem1/solve_problem_1.exs
+++ problem1/solve_problem_1.exs
@@ -3,8 +3,8 @@ defmodule EulerProblemOne do
   def calculate(3), do: 3
   def calculate(n), do: filter(n) + calculate(n - 1)
 
-  def filter(n) when rem(n, 3) == 0 or rem(n, 5) == 0, do: n
-  def filter(_), do: 0
+  defp filter(n) when rem(n, 3) == 0 or rem(n, 5) == 0, do: n
+  defp filter(_), do: 0
 
 end
 
