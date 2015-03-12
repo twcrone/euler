f2401f165ffd4733ed9fd3cde7d8c52dde10bc5b
Todd Crone <todd.crone@perceptivesoftware.com>
2015-03-11 21:39:19 -0400

    Minor refactor

--- problem1/solve_problem_1.exs
+++ problem1/solve_problem_1.exs
@@ -1,12 +1,11 @@
 defmodule EulerProblemOne do
 
   def calculate(3), do: 3
-  def calculate(n) do
-    calculate(n - 1) + filter(n)
-  end
+  def calculate(n), do: filter(n) + calculate(n - 1)
 
   def filter(n) when rem(n, 3) == 0 or rem(n, 5) == 0, do: n
   def filter(_), do: 0
+
 end
 
 answer = EulerProblemOne.calculate(999)
