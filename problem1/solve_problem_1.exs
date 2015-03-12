9e592aa9498532ee336e6028da7355e7002e495b
Todd Crone <todd.crone@perceptivesoftware.com>
2015-03-11 23:07:15 -0400

    Refactor to use reduce

--- problem1/solve_problem_1.exs
+++ problem1/solve_problem_1.exs
@@ -1,12 +1,10 @@
 defmodule EulerProblemOne do
 
-  def calculate(3), do: 3
-  def calculate(n), do: filter(n) + calculate(n - 1)
+  def solve(n), do: Enum.reduce(1..n, 0, &sum/2)
 
-  defp filter(n) when rem(n, 3) == 0 or rem(n, 5) == 0, do: n
-  defp filter(_), do: 0
+  defp sum(n, sum) when rem(n, 3) == 0 or rem(n, 5) == 0, do: sum + n
+  defp sum(_, sum), do: sum
 
 end
 
-answer = EulerProblemOne.calculate(999)
-IO.puts(answer)
+IO.puts(EulerProblemOne.solve(999))
