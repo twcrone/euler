f9a90aa99d30abd1793f134fb2533a9f900c4006
Todd Crone <todd.crone@perceptivesoftware.com>
2015-08-17 11:31:22 -0400

    Remove unneeded module qualifications

--- problem3/problem3.exs
+++ problem3/problem3.exs
@@ -8,8 +8,8 @@ defmodule Problem3Test do
   end
 
   def is_prime_factor_of?(num, base) do
-  	if Problem3Test.is_prime?(num) do
-  		Problem3Test.is_factor_of?(num, base)
+  	if is_prime?(num) do
+  		is_factor_of?(num, base)
   	else
   		false
   	end
@@ -18,69 +18,69 @@ defmodule Problem3Test do
   def is_prime?(num, factor) do
   	cond do
   	  num == 2 -> true
-	  Problem3Test.is_factor_of?(factor, num) -> false
-	  factor < (num/factor) -> is_prime?(num, factor + 1)
-	  true -> true
-	end
+      is_factor_of?(factor, num) -> false
+	    factor < (num/factor) -> is_prime?(num, factor + 1)
+	    true -> true
+    end
   end  
 
   def is_prime?(num) do
-  	Problem3Test.is_prime?(num, 2)
+  	is_prime?(num, 2)
   end
 
   def get_largest_prime_factor(num) do
-  	Problem3Test.get_largest_prime_factor(num, 2)
+  	get_largest_prime_factor(num, 2)
   end
 
   def get_largest_prime_factor(num, divisor) do
-  	if Problem3Test.is_factor_of?(divisor, num) do
+  	if is_factor_of?(divisor, num) do
   		next_candidate = Kernel.div(num, divisor)
   		#IO.puts("#{num}, #{divisor}, #{next_candidate}")
-  		if Problem3Test.is_prime?(next_candidate) do
+  		if is_prime?(next_candidate) do
   			next_candidate
   		else
-  			Problem3Test.get_largest_prime_factor(num, divisor + 1)
+  			get_largest_prime_factor(num, divisor + 1)
   		end
   	else
-		Problem3Test.get_largest_prime_factor(num, divisor + 1)
+		get_largest_prime_factor(num, divisor + 1)
   	end
   end
 
   test "get largest prime factor for 600851475143" do
-  	answer = Problem3Test.get_largest_prime_factor(600851475143)
+  	answer = get_largest_prime_factor(600851475143)
   	IO.puts "Answer is #{answer}"
   	assert answer > 0
   end
 
   test "get largest prime factor" do
-  	assert Problem3Test.is_prime?(29)
-  	assert Problem3Test.is_factor_of?(29, 13195)
-  	assert Problem3Test.is_prime_factor_of?(29, 13195)
-  	assert Problem3Test.get_largest_prime_factor(13195) == 29
+  	assert is_prime?(29)
+  	assert is_factor_of?(29, 13195)
+  	assert is_prime_factor_of?(29, 13195)
+  	assert get_largest_prime_factor(13195) == 29
   end
 
   test "is a factor" do
-    assert Problem3Test.is_factor_of?(2, 4)
+    assert is_factor_of?(2, 4)
   end
 
   test "is not a factor" do
-    assert Problem3Test.is_factor_of?(3, 4) == false
+    assert is_factor_of?(3, 4) == false
   end
 
   test "is prime base case" do
-  	assert Problem3Test.is_prime?(2)
+  	assert is_prime?(2)
   end
 
   test "is prime" do
-  	assert Problem3Test.is_prime?(5)
+  	assert is_prime?(5)
   end
 
   test "is not prime" do
-  	assert Problem3Test.is_prime?(4) == false
+  	assert is_prime?(4) == false
   end
 
   test "is not prime more in depth" do
-  	assert Problem3Test.is_prime?(333) == false
+  	assert is_prime?(333) == false
   end
 
 end
