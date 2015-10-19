41aba4d5cd648a413a2bfe53c775453f357cece5
Todd Crone <todd.crone@perceptivesoftware.com>
2015-10-18 21:37:55 -0400

    Add partial function creator

--- problem5/smallest-multiple.clj
+++ problem5/smallest-multiple.clj
@@ -3,10 +3,10 @@
 
 (def nums '(1 2 3 4 5 6 7 8 9 10))
 
-(defn is-divisible-by? [d]
-  (= (mod 2520 d) 0))
+(defn is-divisible-by? [n d]
+  (= (mod n d) 0))
 
-(defn solve [] (every? is-divisible-by? nums))
+(defn solve [] (every? (partial is-divisible-by? 100) nums))
 
 (deftest base-case
   (is (solve)))
