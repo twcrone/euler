75990efe26a88a02b2b1f38b740784ed7fadd81f
Todd Crone <todd.crone@perceptivesoftware.com>
2015-10-19 12:57:48 -0400

    Add doc comments

--- problem5/smallest-multiple.clj
+++ problem5/smallest-multiple.clj
@@ -1,6 +1,20 @@
 (ns smallest-multiple)
 (use 'clojure.test)
 
+; Project Euler
+;
+; Smallest Multiple
+;
+; Problem 5
+;
+; 2520 is the smallest number that can be divided by each of the numbers from 1
+; to 10 without any remainder.
+;
+; What is the smallest positive number that is evenly divisible by all of the 
+; numbers from 1 to 20?
+;
+; Solution by twcrone
+
 (defn is-divisible-by? [n d]
   (= (mod n d) 0))
 
@@ -16,10 +30,11 @@
       candidate)))
   
 
-; (deftest base-case
-;   (= (solve 10) 2520))
+(deftest base-case
+  (= (solve 10) 2520))
 
-; (run-tests 'smallest-multiple)
-(println "Solution for 10 is " (solve 10))
+(run-tests 'smallest-multiple)
 
+(println "------------------------------")
+(println "Solving for 20...")
 (println "Solution for 20 is " (solve 20))
