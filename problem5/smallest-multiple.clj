abb3a4b21b11cb27ac04a4451061347e761e509e
Todd Crone <todd.crone@perceptivesoftware.com>
2015-10-18 21:32:26 -0400

    Add basic solve

--- problem5/smallest-multiple.clj
+++ problem5/smallest-multiple.clj
@@ -1,10 +1,14 @@
 (ns smallest-multiple)
 (use 'clojure.test)
 
-(defn solve-for [n]
-  0)
+(def nums '(1 2 3 4 5 6 7 8 9 10))
+
+(defn is-divisible-by? [d]
+  (= (mod 2520 d) 0))
+
+(defn solve [] (every? is-divisible-by? nums))
 
 (deftest base-case
-  (is (= (solve-for 10) 2520)))
+  (is (solve)))
 
 (run-tests 'smallest-multiple)
