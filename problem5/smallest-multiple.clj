d9934f996b5ef44c4baf5cfa832f658d50bfcc8a
Todd Crone <todd.crone@perceptivesoftware.com>
2015-10-18 22:20:08 -0400

    Solution

--- problem5/smallest-multiple.clj
+++ problem5/smallest-multiple.clj
@@ -1,14 +1,25 @@
 (ns smallest-multiple)
 (use 'clojure.test)
 
-(def nums '(1 2 3 4 5 6 7 8 9 10))
-
 (defn is-divisible-by? [n d]
   (= (mod n d) 0))
 
-(defn solve [n] (every? (partial is-divisible-by? n) nums))
+(defn solution? [n top]
+  (let [nums (range 1 top)] 
+    (every? (partial is-divisible-by? n) nums)))
 
-(deftest base-case
-  (is (solve 2520)))
+(defn solve [top]
+  (loop [candidate (* top 2)]
+    (if (not (solution? candidate top))
+      (do 
+        (println (+ candidate top))
+        (recur (+ candidate top)))
+      candidate)))
+  
 
-(run-tests 'smallest-multiple)
+; (deftest base-case
+;   (= (solve 10) 2520))
+
+; (run-tests 'smallest-multiple)
+
+(solve 20)
