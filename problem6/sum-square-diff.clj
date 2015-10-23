78ab5bcec421d6a21d371b7895be92760398e08a
Todd Crone <todd.crone@perceptivesoftware.com>
2015-10-23 17:11:53 -0400

    Add Clojure solution to problem 6

---
+++ problem6/sum-square-diff.clj
@@ -0,0 +1,21 @@
+(ns sum-square-diff)
+
+(defn square [n] (* n n))
+
+(defn sum-squares [upper]
+  (reduce + (map square (range (+ upper 1)))))
+
+(defn square-sum [upper]
+  (let [sum (reduce + (range (+ upper 1)))]
+    (* sum sum)))
+
+(defn diff-sums [upper]
+  (- (square-sum upper) (sum-squares upper)))
+
+(sum-squares 10)
+
+(square-sum 10)
+
+(diff-sums 10)
+
+(diff-sums 100)
