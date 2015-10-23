bdd94e3edb01f0258d694d8c8465c23cac8811de
Todd Crone <todd.crone@perceptivesoftware.com>
2015-10-23 17:23:15 -0400

    Refactor and document Clojure solution for problem6

--- problem6/sum-square-diff.clj
+++ problem6/sum-square-diff.clj
@@ -1,4 +1,25 @@
 (ns sum-square-diff)
+(use 'clojure.test)
+
+; Project Euler
+;
+; Sum square difference
+;
+; Problem 6
+;
+; The sum of the squares of the first ten natural numbers is,
+
+; 1^2 + 2^2 + ... + 10^2 = 385
+; The square of the sum of the first ten natural numbers is,
+
+; (1 + 2 + ... + 10)^2 = 552 = 3025
+; Hence the difference between the sum of the squares of the first ten natural
+; numbers and the square of the sum is 3025 − 385 = 2640.
+
+; Find the difference between the sum of the squares of the first one hundred
+; natural numbers and the square of the sum.
+;
+; Solution by twcrone
 
 (defn square [n] (* n n))
 
@@ -12,10 +33,23 @@
 (defn diff-sums [upper]
   (- (square-sum upper) (sum-squares upper)))
 
-(sum-squares 10)
 
-(square-sum 10)
+; tests
 
-(diff-sums 10)
+(deftest sum-squares-10
+  (is (= (sum-squares 10) 385)))
 
-(diff-sums 100)
+(deftest square-sum-10
+  (is (= (square-sum 10) 3025)))
+
+(deftest diff-sums-10
+  (is (= (diff-sums 10) 2640)))
+
+(run-tests 'sum-square-diff)
+
+
+; main
+
+(println "------------------------------")
+(println "Solving for 100...")
+(println "Solution for 100 is " (diff-sums 100))
