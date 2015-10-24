e32ef147978dcb4aaf70142357713e40ceb41051
Todd Crone <todd.crone@perceptivesoftware.com>
2015-10-24 15:56:36 -0400

    Add documentation

--- clojure/problem3.clj
+++ clojure/problem3.clj
@@ -1,8 +1,25 @@
+(ns largest-prime-factor)
+(use 'clojure.test)
+
+; Project Euler
+;
+; Largest Prime Factor
+;
+; Problem 3
+;
+; The prime factors of 13195 are 5, 7, 13 and 29.
+;
+; What is the largest prime factor of the number 600851475143 ?
+;
+; Solution by twcrone
+
 (defn is-prime [n] (not (some #(= (mod n %) 0) (range 2 (/ n 2)))))
 
 (defn is-factor [n d] (= (mod n d) 0))
 
 (defn prime-factors [n]
-  (last (filter #(and (is-factor n %) (is-prime %)) (range 2 n))))
+    (last (filter #(and (is-factor n %) (is-prime %)) (range 2 n))))
 
 (println (prime-factors 13195))
+
+(println (prime-factors 600851475143))
