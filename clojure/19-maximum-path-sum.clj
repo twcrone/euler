bfae6c802591430a512cb72213b084708797ec60
Todd Crone <todd.crone@perceptivesoftware.com>
2015-11-15 09:44:10 -0500

    Add reduce row

--- clojure/19-maximum-path-sum.clj
+++ clojure/19-maximum-path-sum.clj
@@ -3,10 +3,22 @@
 
 (def base-data (list 3 7 4 2 4 6 8 9 3))
 
+(defn reduce-row [values]
+  (loop [src values
+        dest []]
+    (if (= (count src) 1)
+      dest
+      (recur (rest src) (conj dest (max (first src) (second src)))))))
+
+(reduce-row [8 5 9 3])
+
 (defn solve-for [data] 23)
 
 ; test
 (deftest base-case
-  (is (= (solve-for '(3 7 4 2 4 6 8 9 3)) 23)))
+  (is (= (solve-for [3 7 4 2 4 6 8 9 3]) 23)))
+
+(deftest reduce-row-test
+  (is (= (reduce-row [8 5 9 3]) [8 9 9])))
 
 (run-tests 'max-path-sum-1)
